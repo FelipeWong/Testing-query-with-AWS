99a7f9f9786bd4f0cf168328d0bf7423d30eb8c9
EC2 Default User <ec2-user@ip-172-31-41-115.ec2.internal>
2021-08-09 11:00:51 +0000

    review question 2 top 10 query

--- Q2/top 10 like.sql
+++ Q2/top 10 like.sql
@@ -1,4 +1,7 @@
 SELECT articles.title, articles.ID, COUNT(clickstream.objectId) AS like_received 
 FROM articles, clickstream 
 WHERE articles.ID = clickstream.objectId AND 
-clickstream.time BETWEEN "2017-04-01 00:00:00" AND "2017-04-01 23:59:59"
+clickstream.time BETWEEN "2017-04-01 00:00:00" AND "2017-04-01 23:59:59" 
+GROUP BY articles.title, articles.ID 
+ORDER BY like_received DESC 
+LIMIT 10;
